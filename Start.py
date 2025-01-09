0c1eaade30686f1649d9c5c40076ceaeed906541
Berj Dekramanjiane <berj.dekramanjian@gmail.com>
2025-01-09 20:30:30 +0100

    second

--- Start.py
+++ Start.py
@@ -1,1 +1,14 @@
 print("Lets see how this goes")
+
+
+import pandas as pd
+
+data = {
+    "Study": ["Study 1", "Study 2", "Study 3"],
+    "Focus": ["Focus 1", "Focus 2", "Focus 3"],
+    "Research Question": ["Question 1", "Question 2", "Question 3"]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
